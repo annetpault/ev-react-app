4cc9ec77c0e58d3c128d80e1a36ba2591347177c
annetpault <annetmca25@gmail.com>
2026-07-15 02:47:03 +0530

    Updated

--- src/components/ViewVehicle.jsx
+++ src/components/ViewVehicle.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 
 import axios from 'axios'
+import NavigationBar from './NavigationBar'
 
 
 
@@ -24,7 +25,7 @@ const ViewVehicle = () => {
 
     return (
     <div className="container">
-       
+    <NavigationBar />   
     <div className="row">
         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
